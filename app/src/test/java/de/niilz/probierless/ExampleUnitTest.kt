d2598f2c4095651bfdf9f7c3157c394b31e559e9
niilz <niilz@outlook.de>
2025-01-12 20:56:34 +0100

    Test initiating store an reading data from it

--- app/src/test/java/de/niilz/probierless/ExampleUnitTest.kt
+++ app/src/test/java/de/niilz/probierless/ExampleUnitTest.kt
@@ -1,8 +1,18 @@
 package de.niilz.probierless
 
+import de.niilz.probierless.storage.EclipseStore
+import de.niilz.probierless.storage.StoreRoot
+import de.niilz.probierless.tracking.dto.Drink
+import org.eclipse.store.storage.embedded.types.EmbeddedStorageManager
+import org.junit.AfterClass
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertNotNull
+import org.junit.Assert.fail
+import org.junit.BeforeClass
 import org.junit.Test
-
-import org.junit.Assert.*
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Path
 
 /**
  * Example local unit test, which will execute on the development machine (host).
@@ -10,8 +20,63 @@ import org.junit.Assert.*
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-  @Test
-  fun addition_isCorrect() {
-    assertEquals(4, 2 + 2)
-  }
+
+    companion object {
+
+        private const val TEST_DATA_PATH = "./tmp/test-data";
+        private const val TEST_NAME = "test-name"
+
+        @JvmStatic
+        @BeforeClass
+        fun init() {
+            val store = launchDb()
+            val storeRoot = StoreRoot()
+            store.setRoot(storeRoot)
+            store.storeRoot()
+
+            storeRoot.drinks[TEST_NAME] = Drink("foo", "icon")
+            store.store(storeRoot.drinks)
+
+            store.shutdown()
+        }
+
+        @JvmStatic
+        @AfterClass
+        fun cleanup() {
+            deleteTestData(TEST_DATA_PATH)
+        }
+
+        private fun deleteTestData(path: String) {
+            File(path).listFiles()?.forEach {
+                if (it.isDirectory) {
+                    println("Dir: ${it.path}")
+                    deleteTestData(it.path)
+                } else {
+                    println("file: ${it.path}")
+                    it.delete()
+                }
+            }
+            Files.delete(Path.of(path))
+        }
+
+        private fun launchDb(): EmbeddedStorageManager {
+            val db = EclipseStore(TEST_DATA_PATH)
+            val store = db.store
+            store.start()
+            return store
+        }
+    }
+
+    @Test
+    fun dataLoads() {
+        when (val storeRoot = launchDb().root()) {
+            is StoreRoot -> {
+                val testData = storeRoot.drinks[TEST_NAME]
+                assertNotNull(testData)
+                assertEquals("foo", testData?.name)
+            }
+
+            else -> fail("storeRoot is not a StoreRoot")
+        }
+    }
 }
