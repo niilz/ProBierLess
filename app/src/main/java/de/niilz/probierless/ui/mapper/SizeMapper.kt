1d2016d782a647a5c0942db085e8c2431ebd0eb7
niilz <niilz@outlook.de>
2025-02-09 21:26:37 +0100

    Codesmell lift try inline with return

--- app/src/main/java/de/niilz/probierless/ui/mapper/SizeMapper.kt
+++ app/src/main/java/de/niilz/probierless/ui/mapper/SizeMapper.kt
@@ -7,14 +7,14 @@ import de.niilz.probierless.tracking.dto.Ml
 
 fun fromUi(size: String, type: String): Result<DrinkSize> {
     val sizeCleaned = size.trim().replace(",", ".")
-    try {
-        return when (type.lowercase()) {
+    return try {
+        when (type.lowercase()) {
             "ml" -> Result.success(Ml(sizeCleaned.toInt()))
             "cl" -> Result.success(Cl(sizeCleaned.toInt()))
             "l" -> Result.success(L(sizeCleaned.toFloat()))
             else -> Result.failure(IllegalArgumentException("Unsupported drink-size-type: $type"))
         }
     } catch (e: NumberFormatException) {
-        return Result.failure(e)
+        Result.failure(e)
     }
 }
