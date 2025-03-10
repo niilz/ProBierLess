8045c8840c9455c1760d5f638a080fe2ecccffd2
niilz <niilz@outlook.de>
2025-03-10 19:12:46 +0100

    correct test-repo-impl

--- app/src/androidTest/java/de/niilz/probierless/tracking/repository/DrinkRepositoryTestImpl.kt
+++ app/src/androidTest/java/de/niilz/probierless/tracking/repository/DrinkRepositoryTestImpl.kt
@@ -1,14 +1,22 @@
 package de.niilz.probierless.tracking.repository
 
-class DrinkRepositoryTestImpl : DrinkRepository {
-    private val allDrinks = mutableListOf<DrinkDto>()
+import de.niilz.probierless.ui.data.Drink
 
-    override fun fetchAllDrinks(): List<DrinkDto> {
+class DrinkRepositoryTestImpl : DrinkRepository {
+    private val allDrinks = mutableListOf<Drink>()
+
+    override fun fetchAllDrinks(): List<Drink> {
         return allDrinks
     }
 
-    override fun addDrink(drink: DrinkDto) {
+    override fun addDrink(drink: Drink): Int {
+        val id = allDrinks.size
         allDrinks.add(drink)
+        return id
+    }
+
+    override fun removeDrink(id: Int) {
+        allDrinks.removeAt(id)
     }
 
     override fun clearAllDrinks() {
