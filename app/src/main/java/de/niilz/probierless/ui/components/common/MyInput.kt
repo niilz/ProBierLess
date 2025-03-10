d924bda8575778890dfaa51f0dd0babad010a070
niilz <niilz@outlook.de>
2025-03-10 14:48:27 +0100

    add a todo for horizontal alignment of label and input text

--- app/src/main/java/de/niilz/probierless/ui/components/common/MyInput.kt
+++ app/src/main/java/de/niilz/probierless/ui/components/common/MyInput.kt
@@ -2,6 +2,8 @@ package de.niilz.probierless.ui.components.common
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -11,11 +13,13 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import de.niilz.probierless.ui.theme.ProBierLessTheme
 
 @Composable
@@ -30,8 +34,9 @@ fun MyInput(
 
     var value by remember { mutableStateOf("") }
 
+    // TODO: align label and text horizontally.center
     OutlinedTextField(
-        label = { Text(label) },
+        label = { Text(label, Modifier.wrapContentHeight(Alignment.CenterVertically)) },
         keyboardOptions = KeyboardOptions(keyboardType = keyboardType),
         value = value,
         textStyle = textStyle,
@@ -52,6 +57,7 @@ fun AppPreview() {
         Column {
             MyInput(onUpdate = {})
             MyInput(label = "Label", onUpdate = {})
+            MyInput(Modifier.height(100.dp), label = "Label", onUpdate = {})
         }
     }
 }
