521efe49ee1374362a19edf63fa1d3fb660a45ce
niilz <niilz@outlook.de>
2025-05-17 11:51:34 +0300

    Remove redundant overrides Drink is data class again

--- app/src/main/java/de/niilz/probierless/ui/data/Drink.kt
+++ app/src/main/java/de/niilz/probierless/ui/data/Drink.kt
@@ -24,35 +24,4 @@ data class Drink(
         Drink(this.name, this.icon, this.drinkSize, this.vol, this.count + 1)
 
     fun getCount() = count
-
-
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as Drink
-
-        if (vol != other.vol) return false
-        if (count != other.count) return false
-        if (name != other.name) return false
-        if (icon != other.icon) return false
-        if (drinkSize != other.drinkSize) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        var result = vol.hashCode()
-        result = 31 * result + count
-        result = 31 * result + name.hashCode()
-        result = 31 * result + icon.hashCode()
-        result = 31 * result + drinkSize.hashCode()
-        return result
-    }
-
-    override fun toString(): String {
-        return "Drink(name='$name', icon='$icon', drinkSize=$drinkSize, vol=$vol, count=$count)"
-    }
-
-
 }
