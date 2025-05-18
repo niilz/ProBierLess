259c4cb205f017105320b076b0f933977f972a1a
niilz <niilz@outlook.de>
2025-05-18 15:26:12 +0300

    Prepare setup for Config

--- app/src/main/java/de/niilz/probierless/ui/components/Settings.kt
+++ app/src/main/java/de/niilz/probierless/ui/components/Settings.kt
@@ -8,14 +8,18 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import de.niilz.probierless.dev.preview.addDrinks
 import de.niilz.probierless.dev.preview.initDrinkRepositoryForPreview
+import de.niilz.probierless.tracking.viewmodel.SettingsViewModel
 import de.niilz.probierless.ui.components.common.MyButton
 import de.niilz.probierless.ui.components.common.MyInput
 import de.niilz.probierless.ui.navigation.NavControllerManager
@@ -27,8 +31,8 @@ fun Settings(
     innerPadding: PaddingValues = PaddingValues(0.dp),
     navController: NavHostController? = null
 ) {
-    //val settingsViewModel = viewModel<SettingsViewModel>()
-    //val drinks by drinkStateViewModel.drinkState.collectAsState()
+    val settingsViewModel = viewModel<SettingsViewModel>()
+    val drinks by settingsViewModel.settingsState.collectAsState()
 
     Column(
         modifier = Modifier
@@ -37,7 +41,16 @@ fun Settings(
         verticalArrangement = Arrangement.SpaceAround,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Text("Wähle wie viele Getränke du pro Tag trinken möchtest\n(Klicke auf die Counter)", Modifier.fillMaxWidth(.9f), textAlign = TextAlign.Center)
+        Text(
+            "Wähle wie viele Getränke du pro Tag trinken möchtest\n(Klicke auf die Counter)",
+            Modifier.fillMaxWidth(.9f),
+            textAlign = TextAlign.Center
+        )
+        DrinkGrid(
+            drinks = drinks!!,
+            countDrink = TODO(),
+            deleteDrink = TODO(),
+        )
         MyInput(label = "Tage pro Woche", onUpdate = {/* TODO */ })
         // Display drink grid without any counts
         // Display total amount of alkohol allowed
